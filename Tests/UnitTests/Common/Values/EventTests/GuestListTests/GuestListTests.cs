9d555ceb3ad7de6146e9695b129e23164d2f8bf7
Emil Brügge <equistgaard@yahoo.dk>
2025-03-14 15:29:05 +0100

    Added GuestList Tests

--- Tests/UnitTests/Common/Values/EventTests/GuestListTests/GuestListTests.cs
+++ Tests/UnitTests/Common/Values/EventTests/GuestListTests/GuestListTests.cs
@@ -1,23 +1,81 @@
 ﻿using VIAEventAssociation.Core.Domain.Aggregates.EventAggregate.GuestList;
 using VIAEventAssociation.Core.Domain.Aggregates.EventAggregate.GuestList.ValueObjects;
+using VIAEventAssociation.Core.Domain.Aggregates.GuestAggregate.ValueObjects;
 using VIAEventAssociation.Core.Tools.OperationResult.OperationResult;
 
 namespace UnitTests.Common.Values.EventTests.GuestListTests;
 
 public class GuestListTests
 {
+    private readonly GuestListId guestListId;
+    private readonly GuestList guestList;
+    private readonly GuestId guestId;
+    
+    public GuestListTests()
+    {
+        guestListId = GuestListId.Create(Guid.NewGuid()).Value;
+        guestList = GuestList.Create(guestListId).Value;
+        guestId = GuestId.Create(Guid.NewGuid()).Value;
+    }
+    
     [Fact]
-    public void Create_WithAllValidData_ReturnsGuestList()
+    public void Create_WithValidId_ReturnsSuccessResult()
     {
         // Arrange
-        var guestListId = GuestListId.Create(Guid.NewGuid()).Value;
+        var id = GuestListId.Create(Guid.NewGuid()).Value;
         
         // Act
-        var guestList = GuestList.Create(guestListId);
-        var guestListResult = ((Success<GuestList>) guestList).Value;
+        var result = GuestList.Create(id);
         
         // Assert
-        Assert.True(guestList.IsSuccess);
-        Assert.Equal(guestListId, guestListResult.Id);
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result is Success<GuestList> success ? success.Value : null);
+    }
+    
+    [Fact]
+    public void Create_NewGuestList_HasZeroGuests()
+    {
+        // Assert
+        Assert.Equal(0, guestList.numberOfGuests);
+    }
+    
+    [Fact]
+    public void AssignToGuestList_WithValidGuestId_ReturnsSuccess()
+    {
+        // Act
+        var result = guestList.AssignToGuestList(guestId);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(1, guestList.numberOfGuests);
+    }
+    
+    [Fact]
+    public void AssignToGuestList_SameGuestTwice_ReturnsFailure()
+    {
+        // Arrange
+        guestList.AssignToGuestList(guestId);
+        
+        // Act
+        var result = guestList.AssignToGuestList(guestId);
+        
+        // Assert
+        Assert.True(result.IsFailure);
+        var resultFailure = Assert.IsType<Failure<None>>(result);
+        Assert.Equal(1, guestList.numberOfGuests);
+        Assert.Contains(resultFailure.Errors, e => e == GuestListErrors.AssignToGuestList.GuestAlreadyAssigned);
+    }
+    
+    [Fact]
+    public void AssignToGuestList_NullGuestId_ReturnsFailure()
+    {
+        // Act
+        var result = guestList.AssignToGuestList(null);
+        
+        // Assert
+        Assert.True(result.IsFailure);
+        var resultFailure = Assert.IsType<Failure<None>>(result);
+        Assert.Equal(0, guestList.numberOfGuests);
+        Assert.Contains(resultFailure.Errors, e => e == GuestListErrors.AssignToGuestList.GuestIsEmpty);
     }
 }
